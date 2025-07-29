ad5d4110ba9904fd11a69f7b719bc97c67d7caa2
Айкелен <Aikelen.030904@gmail.com>
2025-07-29 14:40:18 +0300

    first commit

---
+++ src/main/java/pro/sky/List_product/ListProductApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.List_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/List_product/ListProductApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.List_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
